3858c033b488ac23575fe64c00edde1e7baf422b
thebenoit <95427292+thebenoit@users.noreply.github.com>
2025-04-05 17:00:28 -0400

    Quelques changements

---
+++ My project/Assets/Scripts/DeplacerCube.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacerCube : MonoBehaviour
+{
+    [SerializeField] private Collider _plan;
+    private UnityEngine.AI.NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+         Vector3? pointClique = Utilitaires.DeterminerClic(_plan);
+         if (pointClique != null)
+         {
+            _agent.destination = pointClique.value;
+         }
+         
+        }
+        
+    }
+}
